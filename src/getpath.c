14a353c1506e96460c80baea3bbf5e17182c04b6
mickoissicko <defualtusermick@gmail.com>
2024-04-13 00:34:48 +0200

    import getpath function (from githelper project)

---
+++ src/getpath.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_STR_LEN 4096
+
+#ifndef _WIN32
+    #include <sys/types.h>
+    #include <pwd.h>
+
+    char* Posix_Path()
+    {
+        struct passwd *Home;
+
+        char* Path = NULL;
+
+        if ((Home = getpwuid(getuid())) != NULL)
+        {
+            Path = (char*)malloc(MAX_STR_LEN);
+
+            if (Path != NULL)
+            {
+                strcpy(Path, Home->pw_dir);
+            }
+        }
+
+        return Path;
+    }
+#else
+    char* Win32_Path()
+    {
+        unsigned long LEN = MAX_PATH_LEN;
+
+        char* HomeDirectory = getenv("USERPROFILE");
+
+        return HomeDirectory;
+    }
+#endif
+
