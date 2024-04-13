522a2824142bd791a78c2e0958455d1d34ca02f4
mickoissicko <defualtusermick@gmail.com>
2024-04-13 22:28:08 +0200

    add kill process logic

--- src/handler/killproc.c
+++ src/handler/killproc.c
@@ -1,15 +1,65 @@
-// This is a standalone module
-// A separate executable is produced for this
-// It will be downloaded and run
+#include "../common/consts.h"
 
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+
+#define MAX_LINE_SIZ 8192
+#define MAX_PATH_SIZ 8192
+#define MAX_SIZ 4096
+
+char* Posix_Path();
+char* Win32_Path();
 
 int main()
 {
-    FILE* Process;
-    Process = fopen("process.txt", "r");
+    char* Path = (char*)malloc(MAX_SIZ * sizeof(char));
 
+    // I know I just broke code consistency
+    // Because the whole codebase uses ifdef
+    // But I'm in a rut
+    // Let's switch things up
 
+    #ifndef _WIN32
+        Path = Posix_Path();
+    #else
+        Path = Win32_Path();
+    #endif
+
+    chdir(Path);
+    // If it doesn't exist
+    // The program will crash
+    // And I'm not gonna do shit about it
+    chdir(LOIDR);
+
+    FILE* ProcFile;
+    ProcFile = fopen("ProcFile.txt", "r");
+
+    if (ProcFile == NULL)
+    {
+        printf("Error reading file");
+        exit(1);
+    }
+
+    unsigned long MaxKillBuf = MAX_PATH_SIZ;
+    unsigned long MaxBuf = MAX_PATH_SIZ;
+
+    char KillCommand[MAX_PATH_SIZ];
+    char Ln[MAX_LINE_SIZ];
+
+    fgets(Ln, MaxBuf, ProcFile);
+
+    // Let's switch things up again
+    #ifdef _WIN32
+        snprintf(KillCommand, MaxKillBuf, "taskkill /f /im %s", Ln);
+        system(KillCommand);
+    #else
+        snprintf(KillCommand, MaxKillBuf, "pkill %s", Ln);
+        system(KillCommand);
+    #endif
+
+    fclose(ProcFile);
+    free(Path);
 
     return 0;
 }
