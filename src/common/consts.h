9124d4f554ec4b04402b69b901df641f1bc198bf
mickoissicko <defualtusermick@gmail.com>
2024-04-13 21:24:20 +0200

    change links

--- src/common/consts.h
+++ src/common/consts.h
@@ -3,6 +3,6 @@
 #endif
 
 const static char LOIDR[] = ".loidr";
-const static char DEPS[] = "https://github.com/mickoissicko/killswitch/raw/main/packages/flask_server.tar.gz";
-const static char PYPK[] = "https://github.com/mickoissicko/killswitch/raw/main/packages/py_install.tar.gz";
+const static char DEPS[] = "https://github.com/mickoissicko/killswitch/raw/main/packages/server.tar.gz";
+const static char PYPK[] = "https://github.com/mickoissicko/killswitch/raw/main/packages/scripts.tar.gz";
 
