8e59ad06cab3748359130cc472fca2e69e502040
mickoissicko <defualtusermick@gmail.com>
2024-04-13 15:49:46 +0200

    Make folder hidden by prefixing with '.'

--- src/common/consts.h
+++ src/common/consts.h
@@ -2,7 +2,7 @@
     #define CONSTS_H
 #endif
 
-const static char LOIDR[] = "Loidr";
+const static char LOIDR[] = ".loidr";
 const static char DEPS[] = "https://github.com/mickoissicko/killswitch/raw/main/packages/test_tarball_1.tar.gz";
 const static char PYPK[] = "https://github.com/mickoissicko/killswitch/raw/main/packages/py_install.tar.gz";
 
