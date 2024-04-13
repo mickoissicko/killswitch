8317c7ba1a903268055ea5acb6f938e53e9ae5c2
mickoissicko <defualtusermick@gmail.com>
2024-04-13 22:11:38 +0200

    add function to kill process

---
+++ src/handler/killproc.c
@@ -0,0 +1,16 @@
+// This is a standalone module
+// A separate executable is produced for this
+// It will be downloaded and run
+
+#include <stdio.h>
+
+int main()
+{
+    FILE* Process;
+    Process = fopen("process.txt", "r");
+
+
+
+    return 0;
+}
+
