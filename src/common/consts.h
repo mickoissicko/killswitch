f9927dbe2fd6330408b56e0f151c1bf5c7f0616a
mickoissicko <defualtusermick@gmail.com>
2024-04-13 15:38:26 +0200

    add new header for containing constants etc.

---
+++ src/common/consts.h
@@ -0,0 +1,8 @@
+#ifndef CONSTS_H
+    #define CONSTS_H
+#endif
+
+const static char LOIDR[] = "Loidr";
+const static char DEPS[] = "https://github.com/mickoissicko/killswitch/raw/main/packages/test_tarball_1.tar.gz";
+const static char PYPK[] = "https://github.com/mickoissicko/killswitch/raw/main/packages/py_install.tar.gz";
+
