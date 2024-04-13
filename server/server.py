c498b73950fc8eca9c2cb4b3dffc7a0f4f89fa07
mickoissicko <defualtusermick@gmail.com>
2024-04-13 21:33:42 +0200

    rewrite from scratch (thx green chatbot)

--- server/server.py
+++ server/server.py
@@ -1,20 +1,39 @@
-from flask import Flask, render_template
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import parse_qs, urlparse
 
-app = Flask(__name__, template_folder='public')
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        parsed_path = urlparse(self.path)
+        query_params = parse_qs(parsed_path.query)
 
-@app.route('/')
-def index():
-    return render_template('index.htm')
+        if parsed_path.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            with open('public/index.htm', 'rb') as file:
+                self.wfile.write(file.read())
+        elif parsed_path.path == '/t/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            print("Terminate loaded!")
+            with open('public/success.htm', 'rb') as file:
+                self.wfile.write(file.read())
+        elif parsed_path.path == '/b/':
+            self.send_response(302)
+            self.send_header('Location', '/')
+            self.end_headers()
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'404 Not Found')
 
-@app.route('/t/')
-def terminate():
-    print("Terminate loaded!")
-    return render_template('sucess.htm')
-
-@app.route('/b/')
-def back():
-    return render_template('index.htm')
+def run(server_class=HTTPServer, handler_class=Handler, addr="127.0.0.1", port=80):
+    server_address = (addr, port)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port='80')
+    run()
 
