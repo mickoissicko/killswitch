2eea4d7172bc44c830afc49a701bdd7acf2e02d8
mickoissicko <mickeysidd@proton.me>
2024-04-14 13:18:31 +0200

    fix script, and remove unlimited open

--- scripts/win32_starter.bat
+++ scripts/win32_starter.bat
@@ -6,7 +6,19 @@ REM And on POSIX you can just use 'sudo'
 REM ...lol
 
 SET "params=%*"
-CD /D "%~dp0" && ( IF EXIST "%temp%\getadmin.vbs" DEL "%temp%\getadmin.vbs" ) && FSUTIL dirty QUERY %systemdrive% 1 > NUL 2 > NUL || (  ECHO Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && EXIT /B )
 
+NET FILE 1>NUL 2>NUL
+
+IF %ERRORLEVEL% EQU 0 (
+    GOTO :StartServer
+)
+
+CD /D "%~dp0" && (
+    IF EXIST "%temp%\getadmin.vbs" DEL "%temp%\getadmin.vbs"
+) && FSUTIL dirty QUERY %systemdrive% 1 > NUL 2 > NUL || (
+    ECHO Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && EXIT /B
+)
+
+:StartServer
 python server.py
 
