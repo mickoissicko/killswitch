6f09f96c0947dd5e1b3b7cbdeb08a1baec5a15da
mickoissicko <mickeysidd@proton.me>
2024-04-14 16:02:50 +0200

    add new stuff

--- scripts/win32_installer.bat
+++ scripts/win32_installer.bat
@@ -1,6 +1,11 @@
 @ECHO OFF
 REM Maximum compatibility
 
+python --version >NUL 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    EXIT
+)
+
 wget https://www.python.org/ftp/python/3.11.9/python-3.11.9.exe
 python-3.11.9.exe /quiet PrependPath=1
 
