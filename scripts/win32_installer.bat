af1c71565f67bcd7b5a7ded4a50c56f6fe15ac19
mickoissicko <mickeysidd@proton.me>
2024-04-14 13:01:13 +0200

    Will make lkater

--- scripts/win32_installer.bat
+++ scripts/win32_installer.bat
@@ -1,19 +1,2 @@
-@ECHO OFF
-
-REM Batch sucks
-REM It's dumb
-
-python --version > NUL 2>&1
-
-IF %errorlevel% EQU 0 (
-    EXIT
-)
-
-SETLOCAL
-SET "python_url=https://www.python.org/downloads/release/python-3118/"
-
-POWERSHELL -Command "(New-Object System.Net.WebClient).DownloadFile('%python_url%', 'python_installer.exe')"
-
-python_installer.exe /quiet InstallAllUsers=1 PrependPath=1 Include_launcher=0
-python -m ensurepip
+REM Nothing here yet
 
