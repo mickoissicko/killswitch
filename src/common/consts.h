329a161592765e8888c8044bc7a6814e85d9ce3c
mickoissicko <defualtusermick@gmail.com>
2024-04-18 19:18:10 +0200

    use .mix/loidr/ path

--- src/common/consts.h
+++ src/common/consts.h
@@ -2,7 +2,7 @@
     #define CONSTS_H
 #endif
 
-const static char LOIDR[] = ".loidr";
+const static char LOIDR[] = ".mix/loidr";
 const static char DEPS[] = "https://github.com/mickoissicko/killswitch/raw/main/packages/server.tar.gz";
 const static char PYPK[] = "https://github.com/mickoissicko/killswitch/raw/main/packages/scripts.tar.gz";
 
